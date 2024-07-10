d508e348734cb00be3ce77ff176f746e67bd481e
HenryZeng <zengdeveloper@qq.com>
2024-07-11 05:34:23 +0800

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "RMS build environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        gtk4
+        libadwaita
+        gnome.adwaita-icon-theme
+        pkg-config
+        xmake
+        clang-tools
+        llvmPackages_18.clangUseLLVM
+        # wrapGAppsHook4
+      ];
+      shellHook = ''
+        unset LD
+      '';
+    };
+  };
+}
