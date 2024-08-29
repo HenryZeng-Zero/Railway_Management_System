9f218861e671a3733b48f2dd9b0c48e499d17184
HenryZeng <zengdeveloper@qq.com>
2024-08-29 19:31:39 +0800

    修改编译流程，将直接拷贝资源到编译产物目录的方式修改为通过GResource打包二进制依赖，通过链接的方式和可执行程序整合

--- xmake.lua
+++ xmake.lua
@@ -1,28 +1,22 @@
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
 
-add_requires("libadwaita-1","gobject-2.0")
+add_requires("libadwaita-1","gobject-2.0","gio-2.0")
 
-rule("ResCopy")
-    after_build(function (target)
-        function ResCopy(name) 
-            local proj_dir = os.projectdir()
-            local source_design_path = path.join(proj_dir,name)
-            local target_dir = path.join(proj_dir,target:targetdir())
-            local target_design_path = path.join(target_dir,name)
-            
-            if os.isdir(target_design_path) then
-                os.rm(target_design_path)
-                os.cp(source_design_path,target_design_path)
-            else 
-                os.cp(source_design_path,target_design_path)
-                print()
-            end
-            cprint("=> 同步" .. name .. " 资源目录 ${bright green}✓${clear}")
-        end
-        
-        ResCopy("design")
+add_includedirs("gResource","src","rk-data")
 
+target("gResource")
+    set_kind("static")
+    set_languages("c11")
+    set_optimize("fastest")
+    add_files("gResource/**.c")
+    add_packages("gio-2.0")
+
+    before_build(function (target)
+        local target_dir = path.join(os.projectdir(),"gResource")
+        os.cd(target_dir)
+        os.execv("make",{"clean"})
+        os.execv("make")
     end)
 
 target("rk-data")
@@ -30,17 +24,14 @@ target("rk-data")
     set_languages("c11")
     set_optimize("fastest")
     add_packages("gobject-2.0")
-    add_includedirs("rk-data")
     add_files("rk-data/**.c")
 
 target("Railway_Management_System")
     set_kind("binary")
     set_languages("c11")
     set_optimize("fastest")
-    add_rules("ResCopy")
 
     add_packages("libadwaita-1")
-    add_deps("rk-data")
-    
-    add_includedirs("src","rk-data")
+    add_deps("rk-data","gResource")
+
     add_files("src/**.c")
