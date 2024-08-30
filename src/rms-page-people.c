f5a5ee4905a6894246762aeb196a01467e76a6c5
HenryZeng <zengdeveloper@qq.com>
2024-08-30 22:40:49 +0800

    update: bind

--- src/rms-page-people.c
+++ src/rms-page-people.c
@@ -1,12 +1,34 @@
 #include "rms-page-people.h"
 
+#define GtkColumnViewColumnN(n) GtkColumnViewColumn* col##n;
 struct _RmsPagePeople
 {
     GtkBox parent_type;
+
+    GtkButton* add_data;
+    GtkButton* set_data;
+    GtkButton* del_data;
+
+    GtkColumnView* column_view;
+    GtkColumnViewColumnN(1)
+    GtkColumnViewColumnN(2)
+    GtkColumnViewColumnN(3)
+    GtkColumnViewColumnN(4)
 };
 
 G_DEFINE_TYPE (RmsPagePeople, rms_page_people, GTK_TYPE_BOX)
 
+/* part: constructed */
+
+static void
+rms_page_people_constructed(GObject *gobject){
+    RmsPagePeople *self = RMS_PAGE_PEOPLE(gobject);
+
+    /* 调用父类<加载完成后初始化>函数 */
+    G_OBJECT_CLASS(rms_page_people_parent_class)->constructed(gobject);
+}
+
+/* End */
 
 static void
 rms_page_people_init (RmsPagePeople *self)
@@ -26,10 +48,24 @@ rms_page_people_dispose (GObject *gobject)
 static void
 rms_page_people_class_init (RmsPagePeopleClass *klass)
 {
-    GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+    GObjectClass *oclass = G_OBJECT_CLASS (klass);
+    GtkWidgetClass *wclass = GTK_WIDGET_CLASS (klass);
+    GtkWindowClass *winclass = GTK_WINDOW_CLASS (klass);
 
-    gtk_widget_class_set_template_from_resource (widget_class,
+    gtk_widget_class_set_template_from_resource (wclass,
                                                "/rikki/tools/rms/rms-page-people.ui");
+
+    gtk_widget_class_bind_template_child (wclass, RmsPagePeople, add_data);
+    gtk_widget_class_bind_template_child (wclass, RmsPagePeople, set_data);
+    gtk_widget_class_bind_template_child (wclass, RmsPagePeople, del_data);
+
+    gtk_widget_class_bind_template_child (wclass, RmsPagePeople, column_view);
+    gtk_widget_class_bind_template_child (wclass, RmsPagePeople, col1);
+    gtk_widget_class_bind_template_child (wclass, RmsPagePeople, col2);
+    gtk_widget_class_bind_template_child (wclass, RmsPagePeople, col3);
+    gtk_widget_class_bind_template_child (wclass, RmsPagePeople, col4);
+
+    oclass->constructed = rms_page_people_constructed;
 }
 
 
--- src/rms-page-train.c
+++ src/rms-page-train.c
@@ -1,11 +1,38 @@
 #include "rms-page-train.h"
 
+#define GtkColumnViewColumnN(n) GtkColumnViewColumn* col##n;
 struct _RmsPageTrain
 {
     GtkBox parent_type;
+
+    GtkButton* add_data;
+    GtkButton* set_data;
+    GtkButton* del_data;
+
+    GtkColumnView* column_view;
+    GtkColumnViewColumnN(1)
+    GtkColumnViewColumnN(2)
+    GtkColumnViewColumnN(3)
+    GtkColumnViewColumnN(4)
+    GtkColumnViewColumnN(5)
+    GtkColumnViewColumnN(6)
+    GtkColumnViewColumnN(7)
+    GtkColumnViewColumnN(8)
 };
 
 G_DEFINE_TYPE (RmsPageTrain, rms_page_train, GTK_TYPE_BOX)
+
+/* part: constructed */
+
+static void
+rms_page_train_constructed(GObject *gobject){
+    RmsPageTrain *self = RMS_PAGE_TRAIN(gobject);
+
+    /* 调用父类<加载完成后初始化>函数 */
+    G_OBJECT_CLASS(rms_page_train_parent_class)->constructed(gobject);
+}
+
+/* End */
 
 static void
 rms_page_train_init (RmsPageTrain *self)
@@ -24,10 +51,28 @@ rms_page_train_dispose (GObject *gobject)
 static void
 rms_page_train_class_init (RmsPageTrainClass *klass)
 {
-    GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+    GObjectClass *oclass = G_OBJECT_CLASS (klass);
+    GtkWidgetClass *wclass = GTK_WIDGET_CLASS (klass);
+    GtkWindowClass *winclass = GTK_WINDOW_CLASS (klass);
 
-    gtk_widget_class_set_template_from_resource (widget_class,
+    gtk_widget_class_set_template_from_resource (wclass,
                                                "/rikki/tools/rms/rms-page-train.ui");
+
+    gtk_widget_class_bind_template_child (wclass, RmsPageTrain, add_data);
+    gtk_widget_class_bind_template_child (wclass, RmsPageTrain, set_data);
+    gtk_widget_class_bind_template_child (wclass, RmsPageTrain, del_data);
+
+    gtk_widget_class_bind_template_child (wclass, RmsPageTrain, column_view);
+    gtk_widget_class_bind_template_child (wclass, RmsPageTrain, col1);
+    gtk_widget_class_bind_template_child (wclass, RmsPageTrain, col2);
+    gtk_widget_class_bind_template_child (wclass, RmsPageTrain, col3);
+    gtk_widget_class_bind_template_child (wclass, RmsPageTrain, col4);
+    gtk_widget_class_bind_template_child (wclass, RmsPageTrain, col5);
+    gtk_widget_class_bind_template_child (wclass, RmsPageTrain, col6);
+    gtk_widget_class_bind_template_child (wclass, RmsPageTrain, col7);
+    gtk_widget_class_bind_template_child (wclass, RmsPageTrain, col8);
+
+    oclass->constructed = rms_page_train_constructed;
 }
 
 
