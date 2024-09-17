4f91f4dfea0889c296d12e8b4c6ed08a7667e0fe
HenryZeng <zengdeveloper@qq.com>
2024-09-18 05:36:34 +0800

    update data model

--- src/model-people.c
+++ src/model-people.c
@@ -28,12 +28,17 @@ model_people_class_init(ModelPeopleClass *class)
 
 /* public */
 
-ModelPeople * model_people_new(const char *name,
+ModelPeople * model_people_new()
+{
+    return g_object_new(MODEL_TYPE_PEOPLE, NULL);
+}
+
+ModelPeople * model_people_new_with_values(const char *name,
                 const char *train,
                 const char *to,
                 const char *sign)
 {
-    ModelPeople  *item = g_object_new(MODEL_TYPE_PEOPLE, NULL);
+    ModelPeople *item = g_object_new(MODEL_TYPE_PEOPLE, NULL);
     item->name = g_strdup(name);
     item->train = g_strdup(train);
     item->to = g_strdup(to);
@@ -56,6 +61,26 @@ model_people_get_ById(ModelPeople* self,int id)
         break;
         case MODEL_PEOPLE_SIGN:
             return self->sign;
+        break;
+    }
+    return NULL;
+}
+
+void
+model_people_set_ById(ModelPeople* self,int id,const char* value)
+{
+    switch (id) {
+        case MODEL_PEOPLE_NAME:
+            strcpy(self->name, value);
+        break;
+        case MODEL_PEOPLE_TRAIN:
+            strcpy(self->train, value);
+        break;
+        case MODEL_PEOPLE_TO:
+            strcpy(self->to, value);
+        break;
+        case MODEL_PEOPLE_SIGN:
+            strcpy(self->sign, value);
         break;
     }
 }
